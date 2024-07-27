7f95b73d575df91223665cce6f4ea27577068d5d
Oyin Akinboye <akinboyeoyinkansola@gmail.com>
2024-07-27 17:56:09 -0400

    Fix FontAwesome import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Add this line
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
