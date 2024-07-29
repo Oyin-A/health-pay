a8e1ba073815003331b16af130cf5d558a26d598
Oyin Akinboye <akinboyeoyinkansola@gmail.com>
2024-07-29 15:49:04 -0400

    Add config-overrides.js to polyfill process module

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 module.exports = function override(config, env) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    path: require.resolve('path-browserify')
+    path: require.resolve('path-browserify'),
+    process: require.resolve('process/browser'),
   };
 
   config.plugins = (config.plugins || []).concat([
